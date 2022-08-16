cf0af1cc028383b6b7db316c8d25531296a16b94
Abran Rodriguez Jr <abranrodriguezjr@gmail.com>
2022-08-16 11:18:37 -0500

    ten functions

--- solutions.js
+++ solutions.js
@@ -20,3 +20,58 @@
  * and
  * concat
  */
+//1
+function isTrue(input){
+    return input === true;
+}
+//2
+function isFalse(input){
+    return input === false;
+}
+//3
+function not(input){
+    return !input;
+}
+//4
+function addOne(input){
+    return parseFloat(input) + 1;
+}
+//5
+function isEven(input){
+    if (parseFloat(input) % 2 === 0 ){
+        return true;
+    } else {
+        return false
+    }
+
+}
+//6
+function isIdentical( input1, input2){
+    if (input1 === input2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+//7
+function isEqual( input1, input2){
+    if (input1 == input2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+//8
+function or(input1, input2){
+    return input1 || input2;
+}
+//9
+function and(input1, input2){
+    return input1 && input2;
+}
+//10
+function concat(input1 , input2){
+    input1 = input1.toString()
+    input2 = input2.toString()
+    return input1 + input2;
+}
